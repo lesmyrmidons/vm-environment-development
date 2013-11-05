f8e6075efcd2daf53e28b71c543a47daac92c73a
haodev <haodev@users.noreply.github.com>
2013-11-06 00:28:31 +0100

    Delete parameters.rb

--- parameters.rb
+++
@@ -1,8 +0,0 @@
-module MyVars
-
-    SF_VM_NAME = "symfony2-debian-7.x"
-    PATH_SHARE_FOLDER = "/Volumes/SAVE2/Dev/Wantedeal/"
-    PATH_MOUNT_FOLDER = "/data"
-    ANSIBLE_VERBOSE = "-vvv"
-
-end

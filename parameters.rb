961e267285f071bad283a23b6da133b3789806d3
Hao LAY <haotiger93@free.fr>
2013-11-06 00:25:32 +0100

    Merge with upstream, and some moodif

--- parameters.rb
+++ parameters.rb
@@ -2,6 +2,7 @@ module MyVars
 
     SF_VM_NAME = "symfony2-debian-7.1"
     PATH_SHARE_FOLDER = "/Volumes/SAVE2/Dev/Wantedeal/"
-    ANSIBLE_VERBOSE = "-vvv"
+    PATH_MOUNT_FOLDER = "/data"
+    ANSIBLE_VERBOSE = "-vvvv"
 
 end
